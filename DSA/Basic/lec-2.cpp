5bc6fe7bd827d5fdc0c11feab298e381afbd0ef8
Rohan Singh <117467200+rohan4693@users.noreply.github.com>
2023-11-14 15:12:21 +0530

    Add files via upload

---
+++ DSA/Basic/lec-2.cpp
@@ -0,0 +1,58 @@
+// #include<iostream>
+
+// int main(){
+//     std::cout<<"Namaste Duniya :-)";
+// }
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    // cout<<"Namaste Duniya :-)"<<endl;
+    // int a=4;
+    // cout<<" the value of a="<<a<<endl;
+
+    // float b=4.778;
+    // cout<<" the value of b="<<b<<endl;
+
+    // char c='r';
+    // cout<<" the value of c="<<c<<endl;
+
+    // bool d=true, e=false;
+    // cout<<"the value of d="<<d<<endl<<"the value of e="<<e<<endl;
+
+    // double f=34.456789;
+    // cout<<"the value of f="<<f<<endl;
+
+    // int size=sizeof(char);
+    // cout<<"the size of char is "<<size<<endl;
+
+    // char h=12345678;
+    // cout<<h<<endl;
+
+    // int b=20, c=19;
+
+    // bool a=(b==c);
+    // cout<<a<<endl;
+
+    // bool aa=(b<c);
+    // cout<<aa<<endl;
+
+    // bool bb=(b>c);
+    // cout<<bb<<endl;
+
+    // bool cc=(b>=c);
+    // cout<<cc<<endl;
+
+    // bool dd=(b<=c);
+    // cout<<dd<<endl;
+
+    // bool ee=(b!=c);
+    // cout<<ee<<endl;
+
+    // bool ff=(b=!c);
+    // cout<<ff<<endl;
+    
+    return 0;
+
+}
